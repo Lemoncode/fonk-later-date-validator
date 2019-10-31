9d47730930ffa790bc97f46918186329816e3eaa
Nya Garcia <nyablk97@gmail.com>
2019-10-31 13:38:25 +0100

    feat(validator.spec): add more specs

--- src/validator.spec.ts
+++ src/validator.spec.ts
@@ -40,7 +40,98 @@ describe('fonk-later-date-validator specs', () => {
     });
   });
 
-  it('should return failed validation when value is a valid Date object earlier than customArgs date param', () => {
+  it('should return succeeded validation when value is a valid Date object with year later than customArgs date param', () => {
+    const value = new Date(2019);
+    const date = new Date(2018);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid Date object with year and month later than customArgs date param', () => {
+    const value = new Date(2018, 12);
+    const date = new Date(2018, 11);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid Date object with year, month and days later than customArgs date param', () => {
+    const value = new Date(2018, 11, 23);
+    const date = new Date(2018, 11, 5);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid Date object with year, month, days and hours later than customArgs date param', () => {
+    const value = new Date(2018, 11, 30, 18);
+    const date = new Date(2018, 11, 30, 6);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid Date object with year, month, days, hours and minutes later than customArgs date param', () => {
+    const value = new Date(2018, 12, 24, 10, 45);
+    const date = new Date(2018, 12, 24, 10, 23);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid Date object year, month, days, hours, minutes and seconds later than customArgs date param', () => {
+    const value = new Date(2018, 12, 30, 15, 33, 50);
+    const date = new Date(2018, 12, 30, 15, 33, 40);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid Date object year, month, days, hours, minutes, seconds and milliseconds later than customArgs date param', () => {
+    const value = new Date(2018, 12, 30, 15, 33, 45, 18);
+    const date = new Date(2018, 12, 30, 15, 33, 45, 7);
+
+    const result = validator({ value, customArgs: { date } });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'LATER_DATE',
+    });
+  });
+
+  it('should return failed validation when value is a valid Date object later than customArgs date param', () => {
     const value = new Date(2018, 11, 24, 10, 33, 30, 0);
     const date = new Date(2018, 12, 30, 15, 33, 30, 0);
 
@@ -48,7 +139,7 @@ describe('fonk-later-date-validator specs', () => {
 
     expect(result).toEqual({
       succeeded: false,
-      message: "Date isn't previous to the one provided.",
+      message: "Date isn't later than the one provided.",
       type: 'LATER_DATE',
     });
   });
